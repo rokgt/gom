6288da18989ba769e9fc576b9a0df7429f0adb18
rokgt <knight_gt@naver.com>
2023-09-15 11:36:18 +0900

    0915오전

--- php/tng/88_01_tng_sort.php
+++ php/tng/88_01_tng_sort.php
@@ -5,37 +5,35 @@
 $arr = [234,1,5,8,2,6,15,66];
 
 // asort($arr);
-$count=8;
-for($tmp > $arr; $arr<=7; $arr++){
-for($z=$count-2; $z>=0; $z--){
-	for($i=0;$i<=$z; $i++){
+// $count=8;
+
+// // for($tmp >= $arr; $arr<=7; $arr++){
+// for($z=$count-2; $z>=0; $z--){
+// 	for($i=0;$i<=$z; $i++){
 	
-		if($arr[$i] > $arr[$i+1] ){
-		$tmp = $arr[$i];
-$arr[$i]=$arr[$i+1];
-$arr[$i+1]=$tmp;}
-// echo "{$z}: {$i}\n";
-}
-	
-}	
-}
-// $i=0;
-// $z=count($arr);
-// while($i<=$z){
-// 	$z=count($arr)-1;
-	
-// 	while($z<=0){
-		
-// 		// echo $arr;
-// 		if($i<$i+1){
-// 			$tmp = $arr[$i];
-// 			$arr[$i]=$arr[$i+1];
-// 			$arr[$i+1]=$tmp;}
-// 			echo $arr;
-// 		$i++;
-// 		$z--;
-// 	}
+// 		if($arr[$i] > $arr[$i+1] ){
+// 		$tmp = $arr[$i];
+// $arr[$i]=$arr[$i+1];
+// $arr[$i+1]=$tmp;}
+
 // }
+	
+// }	
+
+$i=0;
+
+while($i<=7){
+	$z=count($arr)-1;
+	
+	while($z<=0){
+		if($arr[$i]>$arr[$i+1]){
+			$tmp = $arr[$i];
+			$arr[$i]=$arr[$i+1];
+			$arr[$i+1]=$tmp;}$z--;
+		}$i++;
+		
+	
+}
 
 		
 	
