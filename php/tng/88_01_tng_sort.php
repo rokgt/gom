f5fe6ccfc6466b06f076ff389da9fe601f85a1f8
rokgt <knight_gt@naver.com>
2023-09-15 17:52:25 +0900

    0915수업

--- php/tng/88_01_tng_sort.php
+++ php/tng/88_01_tng_sort.php
@@ -7,7 +7,7 @@ $arr = [234,1,5,8,2,6,15,66];
 // asort($arr);
 // $count=8;
 
-// // for($tmp >= $arr; $arr<=7; $arr++){
+
 // for($z=$count-2; $z>=0; $z--){
 // 	for($i=0;$i<=$z; $i++){
 	
@@ -20,17 +20,44 @@ $arr = [234,1,5,8,2,6,15,66];
 	
 // }	
 
+// $i=0;
+
+// $z=count($arr);
+// while($i<=7){
+	
+// 	$z=count($arr)-1;
+// 	while($z>0){
+		
+// 		if($arr[$z]<$arr[$z-1])
+// 		{
+// 			$tmp = $arr[$z];
+// 			$arr[$z]=$arr[$z-1];
+// 			$arr[$z-1]=$tmp;
+// 		}$z--;
+			
+// 		}$i++;
+	
+		
+	
+// }
+
 $i=0;
 
+$z=count($arr);
 while($i<=7){
-	$z=count($arr)-1;
 	
-	while($z<=0){
-		if($arr[$i]>$arr[$i+1]){
-			$tmp = $arr[$i];
-			$arr[$i]=$arr[$i+1];
-			$arr[$i+1]=$tmp;}$z--;
+	$z=count($arr)-1;
+	while($z>0){
+		
+		if($arr[$z]<$arr[$z-1])
+		{
+			$tmp = $arr[$z];
+			$arr[$z]=$arr[$z-1];
+			$arr[$z-1]=$tmp;
+		}$z--;
+			
 		}$i++;
+	
 		
 	
 }
@@ -41,8 +68,6 @@ while($i<=7){
 
 
 
-// }
-// echo $arr;
-print_r($arr);
 
+print_r($arr);
 
