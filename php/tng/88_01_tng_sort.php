81ee7d42c07040c3222564809b2af94932581eee
rokgt/gom <knight_gt@naver.com>
2023-09-16 07:55:24 +0900

    0916

--- php/tng/88_01_tng_sort.php
+++ php/tng/88_01_tng_sort.php
@@ -5,37 +5,54 @@
 $arr = [234,1,5,8,2,6,15,66];
 
 // asort($arr);
-$count=8;
-for($tmp > $arr; $arr<=7; $arr++){
-for($z=$count-2; $z>=0; $z--){
-	for($i=0;$i<=$z; $i++){
+// $count=8;
+// for($tmp > $arr; $arr<=7; $arr++){
+// for($z=$count-2; $z>=0; $z--){
+// 	for($i=0;$i<=$z; $i++){
 	
-		if($arr[$i] > $arr[$i+1] ){
-		$tmp = $arr[$i];
-$arr[$i]=$arr[$i+1];
-$arr[$i+1]=$tmp;}
-// echo "{$z}: {$i}\n";
-}
+// 		if($arr[$i] > $arr[$i+1] ){
+// 		$tmp = $arr[$i];
+// $arr[$i]=$arr[$i+1];
+// $arr[$i+1]=$tmp;}
+// // echo "{$z}: {$i}\n";
+// }
 	
-}	
-}
+// }	
+// }
 // $i=0;
 // $z=count($arr);
-// while($i<=$z){
+// while($i<8){
 // 	$z=count($arr)-1;
 	
-// 	while($z<=0){
+// 	while($z>0){
 		
-// 		// echo $arr;
-// 		if($i<$i+1){
-// 			$tmp = $arr[$i];
-// 			$arr[$i]=$arr[$i+1];
-// 			$arr[$i+1]=$tmp;}
-// 			echo $arr;
-// 		$i++;
-// 		$z--;
-// 	}
+		
+// 		if($arr[$z]<$arr[$z-1]){
+// 			$tmp = $arr[$z];
+// 			$arr[$z]=$arr[$z-1];
+// 			$arr[$z-1]=$tmp;}$z--;$i++;
+			
+// 		}
+		
+	
 // }
+$i=0;
+$z=count($arr)-1;
+while($i<$z){
+	$x=0;
+	
+	while($x<$z-$i){
+		
+		
+		if($arr[$x]>$arr[$x+1]){
+			$tmp = $arr[$x];
+			$arr[$x]=$arr[$x+1];
+			$arr[$x+1]=$tmp;}$x++;
+			
+		}
+		$i++;
+	
+}
 
 		
 	
@@ -43,8 +60,7 @@ $arr[$i+1]=$tmp;}
 
 
 
-// }
-// echo $arr;
+
 print_r($arr);
 
 
