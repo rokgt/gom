e74103f8bfa864864eb482e7d2458b3b5e250602
rokgt <knight_gt@naver.com>
2023-10-10 18:05:52 +0900

    1010프로젝트

---
+++ side_project/mini_test/src/890_detail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=tk, initial-scale=1.0">
+	<title>상세페이지</title>
+</head>
+<body>
+	<main>
+
+	</main>
+	<footer>
+		<div></div>
+		<div>🟡</div>
+		<div>TOP</div>
+	</footer>
+</body>
+</html>
