9c17582c7884009aa2ac69684652be4164a4aef3
rokgt <knight_gt@naver.com>
2023-10-23 18:05:42 +0900

    1023js

---
+++ javascript/js/04.js
@@ -0,0 +1,22 @@
+// console.log("반갑습니다. JS파일입니다.");
+
+
+// -----------------
+// 변수(var, let, const)
+// -----------------
+// var: 중복선언 가능, 재할당 가능, 함수레벨 스코프
+// var u_name = "홀길동";
+// console.log(u_name);
+// var u_name = "갑순이";
+// console.log(u_name);
+
+// let : 중복 선언 불가능, 재할당 가능, 블록레벨 스코프
+let u_name = "홀길동";
+console.log(u_name);
+u_name = "갑순이";
+console.log(u_name);
+
+//const: 중복선언 불가능,재할당 불가능, 블록레벨 스코프
+const AGE = 19;
+// AGE = 20;
+ console.log(AGE);
