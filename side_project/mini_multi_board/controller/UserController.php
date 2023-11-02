f2454f053627b3f378f69063c3db63e530734954
rokgt <knight_gt@naver.com>
2023-11-02 18:07:41 +0900

    1102수업

--- side_project/mini_multi_board/controller/UserController.php
+++ side_project/mini_multi_board/controller/UserController.php
@@ -3,8 +3,11 @@
 namespace controller;//불러올 곳이 있는곳을 지정해주는것
 
 class UserController extends ParentsController{
+//class UserController expends ParentsController{	
 	protected function loginGet(){
+	//protectes function loginGet(){	
 		return "view/login.php";
+		//return "
 	}
 
 	// 회원가입 페이지 이동
